19bb4aa675b038c9338f45d06c06961ef6c84636
kseniyaIT-21 <kseniyaosmirko@gmail.com>
2025-04-13 18:38:09 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -15,24 +15,82 @@ document.addEventListener("DOMContentLoaded", () => {
     // * 4. Конец.
     console.log('Скрипт отработал корректно')
 
-const header = document.querySelector('.header');      
+    const header = document.querySelector('.header');
 
-if (header) {                                           
-    console.log('Константа header существует');
-}
+    if (header) {
+        console.log('Константа header существует');
+        const heightHeader = header.offsetHeight;
 
-const heightHeader = header.offsetHeight;           
-
-        document.addEventListener('scroll', () => {         
+        document.addEventListener('scroll', () => {
 
             console.log('Страница скролится');
 
-            let scrollPageY = this.scrollY;  // получаем значение насколько прокрутили страницу
+            let scrollPageY = this.scrollY;
             {
-                if (scrollPageY > heightHeader) {               
-                    header.classList.add('header--background')      
-                    header.classList.remove('header--background')  
+                if (scrollPageY > heightHeader) {
+                    header.classList.add('header--background');
+
+                } else {
+                    header.classList.remove('header--background');
                 }
             }
         });
-    });
+    }
+
+    //модальное окно для войти
+    const welcоmeButtonModal = document.querySelector(".header__login");
+    const modalApplication = document.querySelector(".window");
+    const modalLogin = document.querySelector(".popup__login");
+
+    if (welcоmeButtonModal && modalApplication && modalLogin) {
+        welcоmeButtonModal.addEventListener("click", () => {
+            modalApplication.removeAttribute("hidden");
+        });
+
+        const closeModalButton = document.querySelector(".popup__end");
+        closeModalButton.addEventListener("click", () => {
+            modalApplication.setAttribute("hidden", true);
+        });
+        window.addEventListener("click", (event) => {
+            if (event.target === modalApplication) {
+                modalApplication.setAttribute("hidden", true);
+            }
+        });
+    };
+    const modalButtonReg = document.querySelector(".header__reg");
+    const modalApplicationReg = document.querySelector(".dialog");
+    const modalReg = document.querySelector(".popup__reg");
+    if (modalButtonReg && modalApplicationReg && modalReg) {
+        modalButtonReg.addEventListener("click", () => {
+            modalApplicationReg.removeAttribute("hidden");
+        });
+
+        const closeModalButtonReg = document.querySelector(".popup__close");
+        closeModalButtonReg.addEventListener("click", () => {
+            modalApplicationReg.setAttribute("hidden", true);
+        });
+        window.addEventListener("click", (event) => {
+            if (event.target === modalApplicationReg) {
+                modalApplicationReg.setAttribute("hidden", true);
+            }
+        });
+    };
+    // 3.4.формирование массива
+    const reviewsContainer = document.querySelector(".reviews");
+    if (reviewsContainer) {
+        const dataTitleReviews = [
+            "Афродита Богиня Красоты",
+            "Зевс Бог Грома и Молний",
+            "Дионис Бог Виноделия",
+        ];
+        const titleReviews =
+        reviewsContainer.querySelectorAll(".reviews__subtitle");
+        titleReviews.forEach((item, index) => {
+            item.textContent = dataTitleReviews[index];
+           });
+    }
+
+
+
+
+});
